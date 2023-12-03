9b888b918f9615623165f7695093a8aa51fb358a
gabicerqueira <133364342+gabicerqueira@users.noreply.github.com>
2023-12-03 17:10:03 -0300

    Banner
    Coloquei o banner

--- lojinha.js
+++ lojinha.js
@@ -152,3 +152,43 @@ function buscarProduto() {
         }
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const imagens = ['img/lojinha-inicial.png', 'img/lojinha-inicial2.png'];
+    let indiceAtual = 0;
+
+    function atualizarImagem() {
+      const carrosselContainer = document.getElementById('carrosselContainer');
+      const bolinhasContainer = document.querySelector('.bolinhas');
+
+      // Atualizar bolinhas
+      bolinhasContainer.innerHTML = '';
+      for (let i = 0; i < imagens.length; i++) {
+        const bolinha = document.createElement('div');
+        bolinha.className = `bolinha ${i === indiceAtual ? 'ativa' : ''}`;
+        bolinhasContainer.appendChild(bolinha);
+      }
+
+      // Atualizar posição do carrossel container
+      carrosselContainer.style.transform = `translateX(${-(indiceAtual * 100)}%)`;
+    }
+
+    function proximaImagem() {
+      indiceAtual = (indiceAtual + 1) % imagens.length; // Avançar para o próximo slide
+      atualizarImagem();
+    }
+
+    function imagemAnterior() {
+      indiceAtual = (indiceAtual - 1 + imagens.length) % imagens.length; // Voltar para o slide anterior
+      atualizarImagem();
+    }
+
+    // Configurar intervalo para trocar de imagem a cada 3 segundos
+    setInterval(proximaImagem, 5000);
+
+    // Adicionar evento de clique para voltar para a imagem anterior
+    document.querySelector('.carrossel').addEventListener('click', imagemAnterior);
+
+    // Atualizar a imagem inicial
+    atualizarImagem();
+  });
